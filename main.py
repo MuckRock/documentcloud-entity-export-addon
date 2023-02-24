8cabd8bba8ff8c03e94afcf5c78809b4f28506e4
Mitchell Kotler <mitch@mitchellkotler.net>
2023-02-24 14:06:36 -0500

    init

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
 """
-This is a hello world add-on for DocumentCloud.
-
-It demonstrates how to write a add-on which can be activated from the
-DocumentCloud add-on system and run using Github Actions.  It receives data
-from DocumentCloud via the request dispatch and writes data back to
-DocumentCloud using the standard API
+Export your entities to a CSV
 """
+
+import csv
 
 from documentcloud.addon import AddOn
 
 
-class HelloWorld(AddOn):
-    """An example Add-On for DocumentCloud."""
-
+class EntityExport(AddOn):
     def main(self):
-        """The main add-on functionality goes here."""
-        # fetch your add-on specific data
-        name = self.data.get("name", "world")
 
-        self.set_message("Hello World start!")
+        with open("entities.csv", "w+") as file:
+            writer = csv.writer(file)
+            writer.writerow(
+                [
+                    "document id",
+                    "entity id",
+                    "entity name",
+                    "wikidata id",
+                    "pages",
+                ]
+            )
 
-        # add a hello note to the first page of each selected document
-        for document in self.get_documents():
-            # get_documents will iterate through all documents efficiently,
-            # either selected or by query, dependeing on which is passed in
-            document.annotations.create(f"Hello {name}!", 0)
+            for document in self.get_documents():
+                resp = self.client.get(
+                    f"documents/{document.id}/entities/?expand=entity"
+                )
+                for entity in resp.json()["results"]:
+                    writer.writerow(
+                        [
+                            document.id,
+                            entity["entity"]["id"],
+                            entity["entity"]["name"],
+                            entity["entity"]["wikidata_id"],
+                            ", ".join(
+                                sorted(set(str(o["page"]) for o in entity["occurrences"]))
+                            ),
+                        ]
+                    )
 
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
-            self.upload_file(file_)
-
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
+            self.upload_file(file)
 
 
 if __name__ == "__main__":
-    HelloWorld().main()
+    EntityExport().main()
